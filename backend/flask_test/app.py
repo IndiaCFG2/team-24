8a5489414c6410f3398f59f61a83ba7519bf23a7
narenchandra859 <narenchandra859@gmail.com>
2020-08-09 00:23:44 +0530

    flask backend

---
+++ backend/flask_test/app.py
@@ -0,0 +1,86 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+import sqlite3
+from flask import g
+  
+con = sqlite3.connect("chart1.db")  
+
+app = Flask(__name__)
+Bootstrap(app)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/admin')
+def admin():
+	return render_template("admin.html")
+
+@app.route('/admin/dashboard')
+def dashboard():
+	return render_template("dashboard.html")
+
+@app.route('/admin/grade_wise')
+def grade_wise():
+	return render_template("grade_wise.html")
+
+@app.route("/admin/tables")
+def tables():
+	return render_template("tables.html")
+
+@app.route('/getDailyForCurriculum', methods = ["GET"])
+def getDailyForCurriculum():
+	board = request['board']
+	cur = con.cursor()
+	cur.execute("SELECT Day_1, Day_2, Day_3, Day_4, Day_5, Day_6, Day_7 from school_level where Board = "+board)
+	rows = cur.fetchall()
+	return rows	
+
+@app.rote('.')
+def getMonthlyForCurriculum():
+
+@app.route()...
+def getClicksForCurriculum():
+
+def getCityForCurriculum()
+
+def getTechForCurriculum()
+
+@app.route('/getDailyForCurriculum', methods = ["GET"])
+def getDailyForCurriculum():
+	board = request['board']
+	cur = con.cursor()
+	cur.execute("SELECT Day_1, Day_2, Day_3, Day_4, Day_5, Day_6, Day_7 from school_level where Board = "+board)
+	rows = cur.fetchall()
+	return rows	
+
+@app.rote('.')
+def getMonthlyForCurriculum():
+
+@app.route()...
+def getClicksForCurriculum():
+
+def getCityForCurriculum()
+
+def getTechForCurriculum()
+
+
+def getDailyForGrade():
+	curr = request['curriculum']
+	grade = request['grade']
+
+
+def getDailyForGradeAndWeek():
+	curr
+	grade
+	week
+
+def getDailyforWeek():
+	curr
+	week
+
+
+
+app.run(debug=True)
+
+
---
+++ backend/flask_test/static/js/index.js
@@ -0,0 +1,30 @@
+func = (url) => {
+	console.log(url);
+	console.log("IM ALIVE");
+	var params = {};
+	var parser = document.createElement('a');
+	parser.href = url;
+	var query = parser.search.substring(1);
+	var vars = query.split('&');
+	for (var i = 0; i < vars.length; i++) {
+		var pair = vars[i].split('=');
+		params[pair[0]] = decodeURIComponent(pair[1]);
+	}
+	// pass schoolid to database, get curriculum var
+	// for now for testing index.html?schoolid=CBSE etc
+	var curriculum = "";
+	if (params.schoolid === 'CBSE') {
+		document.getElementById('CBSE').style.display = "block";
+	}
+	else if (params.schoolid === 'KTAKA') {
+		document.getElementById('KTAKA').style.display = "block";
+	}
+	else if (params.schoolid === 'TS') {
+		document.getElementById('TS').style.display = "block";
+	}
+	else {
+		document.getElementById('MAHA').style.display = "block";
+	}
+}
+
+window.onload = func(document.location.search);
