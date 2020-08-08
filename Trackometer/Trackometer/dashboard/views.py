72eda2be0cd78b0cbfc9c408cce320ad88411265
Bogala-Indu <indureddybogala4@gmail.com>
2020-08-08 23:27:19 +0530

    static files changed!

--- Trackometer/Trackometer/dashboard/views.py
+++ Trackometer/Trackometer/dashboard/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def dash(request):
-	return render(request,'dashboard/dashboard.html')
+	return render(request,'dashboard/main/dashboard.html')
